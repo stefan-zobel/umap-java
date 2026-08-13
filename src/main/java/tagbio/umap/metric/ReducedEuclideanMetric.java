dc15e4160d6b7798b82bc234dd76f4d3bdd64dc8
stefan-zobel <stefan-zobel@users.noreply.github.com>
2026-08-13 21:39:56 +0200

    use loop unrolling for the euclidean distance calculation

--- src/main/java/tagbio/umap/metric/ReducedEuclideanMetric.java
+++ src/main/java/tagbio/umap/metric/ReducedEuclideanMetric.java
@@ -23,8 +23,24 @@ public final class ReducedEuclideanMetric extends Metric {
   @Override
   public float distance(final float[] x, final float[] y) {
     //  D(x, y) = \sum_i (x_i - y_i)^2
-    float result = 0;
-    for (int i = 0; i < x.length; ++i) {
+    float sum0 = 0.0F;
+    float sum1 = 0.0F;
+    float sum2 = 0.0F;
+    float sum3 = 0.0F;
+    int i = 0;
+    final int limit = x.length - 3;
+    for (; i < limit; i += 4) {
+      final float d0 = x[i] - y[i];
+      final float d1 = x[i + 1] - y[i + 1];
+      final float d2 = x[i + 2] - y[i + 2];
+      final float d3 = x[i + 3] - y[i + 3];
+      sum0 += d0 * d0;
+      sum1 += d1 * d1;
+      sum2 += d2 * d2;
+      sum3 += d3 * d3;
+    }
+    float result = (sum0 + sum1) + (sum2 + sum3);
+    for (; i < x.length; ++i) {
       final float d = x[i] - y[i];
       result += d * d;
     }
